5f527e57cd55c40daa653e596e267fc3a6e844c5
tina <102860264+tinachelwanii@users.noreply.github.com>
2023-09-20 20:19:08 +0530

    Create iamrich3.dart

---
+++ iamrich3.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for all our flutter apps.
+void main() { 
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(),
+          body: Center(
+           child: Image(
+            image: 
+               NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWJm7YkuR53TP9u5W6a9Wt_pWbC8JtQPDRcQ&usqp=CAU'),
+       ),
+      ),
+     ),
+    ), 
+ );
+}
